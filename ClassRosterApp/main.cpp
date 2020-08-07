bd84b751862216b79d1bdf57c92a4b574c85863c
Joseph Surla <josephsurla@gmail.com>
2020-08-07 09:25:02 +0900

    Initial Commit

---
+++ ClassRosterApp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ClassRosterApp
+//
+//  Created by Joseph Surla on 8/7/20.
+//  Copyright © 2020 Joseph Surla. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
