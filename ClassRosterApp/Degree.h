8730c2153c96e7e1a167a84ea67ca9bc9dc37b65
Joseph Surla <josephsurla@gmail.com>
2020-08-07 09:31:07 +0900

    Create Degree header file and add enum data type DegreeProgram

---
+++ ClassRosterApp/Degree.h
@@ -0,0 +1,17 @@
+//
+//  Degree.h
+//  ClassRosterApp
+//
+//  Created by Joseph Surla on 8/7/20.
+//  Copyright © 2020 Joseph Surla. All rights reserved.
+//
+
+#ifndef Degree_h
+#define Degree_h
+
+#include <stdio.h>
+
+enum DegreeProgram = { SECURITY, NETWORK, SOFTWARE };
+const string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
+
+#endif /* Degree_hpp */
